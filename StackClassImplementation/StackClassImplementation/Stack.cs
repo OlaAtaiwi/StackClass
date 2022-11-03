8150f05f978cdd5a8a6055277e51f73012a6ede5
OlaAtaiwi <58399231+OlaAtaiwi@users.noreply.github.com>
2022-11-03 08:16:30 +0000

    Update Stack.cs

--- StackClassImplementation/StackClassImplementation/Stack.cs
+++ StackClassImplementation/StackClassImplementation/Stack.cs
@@ -7,29 +7,6 @@ using System.Text;
  */
 namespace StackClassImplementation
 {
-    class LinkedNode<T>
-    {
-        public T Data { get; set; }
-        public LinkedNode<T> Next { get; set; }
-	
-        public LinkedNode()
-        {
-            Data = default(T);
-            Next = null;
-        }
-	
-        public LinkedNode(T data)
-        {
-            Data = data;
-            Next = null;
-        }
-	
-        public LinkedNode(T data, LinkedNode<T> next)
-        {
-            Data = data;
-            Next = next;
-        }
-    }
     public class Stack<T>
     {
         public int Count { get; private set; }
