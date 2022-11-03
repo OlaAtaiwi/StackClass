55554dbb5a75e16119c1ca344cfe45c97e02e0ea
OlaAtaiwi <58399231+OlaAtaiwi@users.noreply.github.com>
2022-11-03 08:16:05 +0000

    Create LinkedNode.cs

---
+++ StackClassImplementation/StackClassImplementation/LinkedNode.cs
@@ -0,0 +1,29 @@
+using System;
+
+
+namespace StackClassImplementation
+{
+class LinkedNode<T>
+    {
+        public T Data { get; set; }
+        public LinkedNode<T> Next { get; set; }
+	
+        public LinkedNode()
+        {
+            Data = default(T);
+            Next = null;
+        }
+	
+        public LinkedNode(T data)
+        {
+            Data = data;
+            Next = null;
+        }
+	
+        public LinkedNode(T data, LinkedNode<T> next)
+        {
+            Data = data;
+            Next = next;
+        }
+    }
+}
